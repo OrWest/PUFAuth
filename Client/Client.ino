47cfbfc8e9a8f2a91e3e68902adc1101b87f64ae
Alex Motor <wwweshka@rambler.ru>
2017-03-18 12:29:21 +0300

    Project init.

---
+++ Client/Client.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Client.ino
+ Created:	18.03.2017 12:26:58
+ Author:	Alex Motor
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
